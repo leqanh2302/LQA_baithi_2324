c073096f64467607dd3a6806837657363a072b50
leqanh2302 <leqanh2302@gmail.com>
2024-06-17 13:34:28 +0700

    final commit

--- LQA_BaiThi2324/obj/Debug/net8.0/LQA_BaiThi2324.AssemblyInfo.cs
+++ LQA_BaiThi2324/obj/Debug/net8.0/LQA_BaiThi2324.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LQA_BaiThi2324")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0c52c982adea9bb50881629ec0da0bb8b803d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LQA_BaiThi2324")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LQA_BaiThi2324")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
